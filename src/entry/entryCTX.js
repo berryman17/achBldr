ca60c487f92c1ae73f73f53d371e0826ba927d9d
Ian Berryman <berryman_ian@yahoo.com>
2019-01-23 22:13:30 -0700

    Added fields for entryXXX objects and entryAddenda

--- src/entry/entryCTX.js
+++ src/entry/entryCTX.js
@@ -1,7 +1,103 @@
 
 
-class EntryCTX extends Entry {
-    constructor() {}
-
-    
+class EntryCTX {
+    constructor() {
+        this.fields = fields;
+    }
 }
+
+const fields = {
+    recordTypeCode: {
+        required: true,
+        position: 1,
+        length: 1,
+        patterm: "/(6)/",
+        defaultValue: "6"
+    },
+    transactionCode: {
+        required: true,
+        position: 2,
+        length: 2,
+        patterm: "/[0-9]{2}/",
+        defaultValue: ""
+    },
+    receivingDfiId: {
+        required: true,
+        position: 4,
+        length: 8,
+        patterm: "/[0-9]{8}/",
+        defaultValue: ""
+    },
+    checkDigit: {
+        required: true,
+        position: 12,
+        length: 1,
+        patterm: "/[0-9]/",
+        defaultValue: ""
+    },
+    accountNumber: {
+        required: true,
+        position: 13,
+        length: 17,
+        patterm: "/[0-9\w\- ]{17}/",
+        defaultValue: ""
+    },
+    amount: {
+        required: true,
+        position: 30,
+        length: 10,
+        patterm: "/[0-9]{10}/",
+        defaultValue: 0
+    },
+    identificationNumber: {
+        required: false,
+        position: 40,
+        length: 15,
+        patterm: "/[0-9\w\- ]{15}/",
+        defaultValue: ""
+    },
+    numberOfAddendaRecords: {
+        required: true,
+        position: 55,
+        length: 4,
+        patterm: "/[0-9]{4}/",
+        defaultValue: 0
+    },
+    individualName: {
+        required: true,
+        position: 59,
+        length: 16,
+        patterm: "/[0-9\w\- ]{16}/",
+        defaultValue: ""
+    },
+    reserved: {
+        required: false,
+        position: 75,
+        length: 2,
+        patterm: "/[ ]{2}/",
+        defaultValue: ""
+    },
+    discretionaryData: {
+        required: false,
+        position: 77,
+        length: 2,
+        patterm: "/[0-9\w\- ]{2}/",
+        defaultValue: ""
+    },
+    addendaRecordIndicator: {
+        required: true,
+        position: 79,
+        length: 1,
+        patterm: "/[01]/",
+        defaultValue: "0"
+    },
+    traceNumber: {
+        required: true,
+        position: 80,
+        length: 15,
+        patterm: "/[0-9]{15}/",
+        defaultValue: ""
+    }
+}
+
+module.exports.EntryCTX = EntryCTX;
---
+++ src/entry/entryPPD.js
@@ -0,0 +1,89 @@
+
+
+class EntryPPD {
+    constructor() {
+        this.fields = fields;
+    }
+}
+
+const fields = {
+    recordTypeCode: {
+        required: true,
+        position: 1,
+        length: 1,
+        patterm: "/(6)/",
+        defaultValue: "6"
+    },
+    transactionCode: {
+        required: true,
+        position: 2,
+        length: 2,
+        patterm: "/[0-9]{2}/",
+        defaultValue: ""
+    },
+    receivingDfiId: {
+        required: true,
+        position: 4,
+        length: 8,
+        patterm: "/[0-9]{8}/",
+        defaultValue: ""
+    },
+    checkDigit: {
+        required: true,
+        position: 12,
+        length: 1,
+        patterm: "/[0-9]/",
+        defaultValue: ""
+    },
+    accountNumber: {
+        required: true,
+        position: 13,
+        length: 17,
+        patterm: "/[0-9\w\- ]{17}/",
+        defaultValue: ""
+    },
+    amount: {
+        required: true,
+        position: 30,
+        length: 10,
+        patterm: "/[0-9]{10}/",
+        defaultValue: 0
+    },
+    individualIdNumber: {
+        required: false,
+        position: 40,
+        length: 15,
+        patterm: "/[0-9\w\- ]{15}/",
+        defaultValue: ""
+    },
+    individualName: {
+        required: true,
+        position: 55,
+        length: 22,
+        patterm: "/[0-9\w\- ]{22}/",
+        defaultValue: ""
+    },
+    discretionaryData: {
+        required: false,
+        position: 77,
+        length: 2,
+        patterm: "/[0-9\w\- ]{2}/",
+        defaultValue: ""
+    },
+    addendaRecordIndicator: {
+        required: true,
+        position: 79,
+        length: 1,
+        patterm: "/[01]/",
+        defaultValue: "0"
+    },
+    traceNumber: {
+        required: true,
+        position: 80,
+        length: 15,
+        patterm: "/[0-9]{15}/",
+        defaultValue: ""
+    }
+}
+
+module.exports.EntryPPD = EntryPPD;
